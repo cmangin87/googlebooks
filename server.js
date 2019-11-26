8995c308aa70e307a7b98a6d920c4b786f0b5195
Christopher Mangin <mangin.christopher@gmail.com>
2019-11-26 18:14:46 -0500

    packages run

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,15 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // This is for production use only
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
 app.use(routes);
 
 mongoose.Promise = Promise;
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/google-books-db',
+  process.env.MONGODB_URI || "mongodb://localhost:27017/google-books-db",
   { useNewUrlParser: true }
 );
 
